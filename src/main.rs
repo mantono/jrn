eb9686d0b3a4267c1130bc3b0835103338a75ff3
Anton Österberg <anton.osterberg@gmail.com>
2021-08-18 00:51:10 +0200

    Implement search support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use std::{ffi::OsString, fmt::format, fs::DirEntry, path::PathBuf, process};
+use std::{
+    convert::TryFrom,
+    ffi::OsString,
+    fmt::{format, Display},
+    fs::DirEntry,
+    path::PathBuf,
+    process,
+};
 
 use cfg::Config;
 use chrono::{Date, Utc};
@@ -23,7 +30,7 @@ fn main() {
 
     match cfg.command() {
         cmd::Command::Edit { date } => edit(&cfg, date).unwrap(),
-        cmd::Command::Search { terms } => todo!(),
+        cmd::Command::Search { terms } => search(&cfg, terms).unwrap(),
         cmd::Command::Log { entries } => log(&cfg, entries).unwrap(),
     };
 }
@@ -80,4 +87,71 @@ fn entry_to_string(entry: walkdir::DirEntry) -> Option<String> {
     std::fs::read_to_string(entry.path())
         .ok()
         .map(|content| format!("---{}---\n{}\n", name, content))
+}
+
+fn search(cfg: &Config, terms: Vec<String>) -> std::io::Result<()> {
+    WalkDir::new(cfg.dir())
+        .follow_links(false)
+        .max_depth(1)
+        .sort_by_file_name()
+        .into_iter()
+        .filter_map(|d| d.ok())
+        .filter_map(|f| Entry::try_from(f).ok())
+        .filter(|e: &Entry| e.contains_any(&terms))
+        .for_each(|e| println!("{}", e));
+
+    Ok(())
+}
+
+struct Entry {
+    pub name: String,
+    pub content: String,
+}
+
+impl Entry {
+    pub fn contains(&self, term: &str) -> bool {
+        self.content.to_lowercase().contains(&term.to_lowercase())
+    }
+
+    pub fn contains_any(&self, terms: &Vec<String>) -> bool {
+        terms.iter().any(|term| self.contains(&term))
+    }
+}
+
+impl Display for Entry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("---{}---\n{}\n", self.name, self.content))
+    }
+}
+
+impl TryFrom<walkdir::DirEntry> for Entry {
+    type Error = EntryError;
+
+    fn try_from(value: walkdir::DirEntry) -> Result<Self, Self::Error> {
+        let f = value.file_type();
+        if f.is_dir() {
+            return Err(EntryError::IsDir);
+        } else if f.is_symlink() {
+            return Err(EntryError::IsSymlink);
+        }
+
+        let name: String = match value.file_name().to_os_string().into_string() {
+            Ok(name) => name,
+            Err(_) => {
+                return Err(EntryError::Other(String::from("Unable to convert name of file")))
+            }
+        };
+        let content: String = match std::fs::read_to_string(value.path()) {
+            Ok(content) => content,
+            Err(e) => return Err(EntryError::Other(e.to_string())),
+        };
+        let entry = Entry { name, content };
+        Ok(entry)
+    }
+}
+
+enum EntryError {
+    IsDir,
+    IsSymlink,
+    Other(String),
 }
